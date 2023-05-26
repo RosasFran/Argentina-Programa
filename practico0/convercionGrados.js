c808a0e0203da41b734aafa0ebeeaa1b94f85ca7
Franco <francorosas415@gmail.com>
2023-05-26 17:17:14 -0300

    Agrego el archio convercionGrados.js

---
+++ practico0/convercionGrados.js
@@ -0,0 +1,8 @@
+//let grados_fahrenheit = 50;
+//let grados_celsius = (grados_fahrenheit - 32) * (5/9);
+//console.log(grados_celsius);
+
+
+let grados_fahrenheit = 75;
+let grados_celsius = (grados_fahrenheit - 32) * (5/9);
+console.log(grados_celsius);
