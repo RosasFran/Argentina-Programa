c441cd66cb0dc37e621c27f92d8f53d1a748f5f5
Franco <francorosas415@gmail.com>
2023-05-26 17:21:10 -0300

    Practico n0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,9 @@
+//let base = 5;
+//let altura = 3;
+//let resultado = base * altura;
+//console.log(resultado);
+
+let base = 12.5;
+let altura = 5.3;
+let resultado = base * altura;
+console.log(resultado);
---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+//let numero1 = 5;
+//let numero2 = 7;
+//let numero3 = 9;
+//let promedio = (numero1 + numero2 + numero3) / 3;
+//console.log(promedio);
+
+//let numero1 = -2;
+//let numero2 = -3;
+//let numero3 = -8;
+//let promedio = (numero1 + numero2 + numero3) / 3;
+//console.log(promedio);
+
+let numero1 = -1.2;
+let numero2 = -5.3;
+let numero3 = -3.3;
+let promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio);
