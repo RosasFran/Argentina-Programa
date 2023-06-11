1eb1381de143192694bea1ca48e83b1320e2ff2e
Franco <francorosas415@gmail.com>
2023-06-11 19:14:59 -0300

    Tercer Ejercicio

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,28 @@
+//3_
+
+// A)
+var var1 = 12;
+var var2 = 10;
+var var3 = 14;
+var var4 = 16;
+
+// B)
+var var5 = var1 + var2;
+console.log(var5); 
+
+
+// c)
+var var6 = var4 - var3;
+console.log(var6);
+
+// D)
+var resultadoFinal = var5 * var6;
+console.log(resultadoFinal);
+
+//E)
+let calc = resultadoFinal % 2;
+let esPar = calc === 0;
+console.log(esPar);
+
+//F)
+console.log("Mis variables iniciales fueron :" , + var1, + var2, + var3, + var4, "." + " La respuesta a verificar si el resultado final es par es:" + " " + esPar.toString());
