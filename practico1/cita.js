d8570f35232382088e2cdaa568fe473e8bdaa599
Franco <francorosas415@gmail.com>
2023-06-11 19:24:55 -0300

    Quinto Ejercicio

---
+++ practico1/cita.js
@@ -0,0 +1,14 @@
+// 4_
+
+// A)
+var tamañoDeCita = "Tres tristes tigres comen trigo en un trigal";
+console.log("El tamaño de cita es:" + " " + tamañoDeCita.length);
+
+//B)
+var indice = "tigres comen trigo";
+console.log("El indice de substring es:" + " " + indice);
+
+//C)
+var  citaOriginal = tamañoDeCita;
+let citaRevisada = cita.substring(0, 31);
+console.log(citaRevisada);
---
+++ practico1/frutas.js
@@ -0,0 +1,17 @@
+// 5_
+
+// A)
+var frutas = [];
+console.log(frutas);
+
+// B)
+frutas.push("Manzana", "Banana", "Pera", "Mandarina", "Durazno");
+console.log(frutas);
+
+// C)
+var ultima = frutas.slice(4);
+console.log("La ultima fruta agregada fue:", ultima);
+
+// D)
+var primera = frutas.slice(0,1);
+console.log("La primer fruta agregada fue:", primera);
